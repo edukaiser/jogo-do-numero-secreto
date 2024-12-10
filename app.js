8518a8a313140c5d4f2a4b99c6efbe745d102c6b
Eduardo Marafigo Kaiser <eduardokaiser268@gmail.com>
2024-12-09 21:38:30 -0300

    Alerando limite para 100

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 <<<<<<< HEAD
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -22,7 +22,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto' );
-    exibirTextoNaTela('p', 'Escolha um número entra 1 e 10.');
+    exibirTextoNaTela('p', 'Escolha um número entra 1 e 100.');
 }
 
 exibirMensagemInicial();
